ff56ca34d8ca1d841ed7447b2bdaf1313293aae7
Shang-Lab <s090029@e.ntu.edu.sg>
2025-02-17 11:06:17 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'guided students', 'links']
+const section_names = ['home', 'publications', 'guidedstudents', 'links']
 
 
 window.addEventListener('DOMContentLoaded', event => {
